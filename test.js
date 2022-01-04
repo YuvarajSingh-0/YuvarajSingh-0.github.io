93d10d48eb247b12b75206fc008fb3c5b0e38b14
Yuvaraj Singh <83658757+YuvarajSingh-0@users.noreply.github.com>
2022-01-05 00:39:16 +0530

    Update test.js

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ var isDark=true;
         document.getElementById("main").className="bg-gray-100 transition-all duration-1000";
         document.getElementById("button").className="rounded-xl hover:border-orange-400 hover:bg-orange-300 cursor-pointer text-black p-2 bg-orange-600 transition-all duration-1000"
         document.getElementsByTagName("h1")[0].innerText="Light Theme"
-        document.getElementsByTagName("h1")[0].className="transform -translate-y-5 text-black transition ease-in-out duration-1000"
+        document.getElementsByTagName("h1")[0].className="transform -translate-y-10 text-black transition ease-in-out duration-1000"
         isDark=false
     }
     else{
