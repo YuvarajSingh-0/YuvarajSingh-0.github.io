0a40bcaa64d7184df863cacb1acc2eeb5124145b
yuvarajsingh-0 <yuvarajsingh170@gmail.com>
2022-01-05 00:08:04 +0530

    Flexbox Usage

--- test.js
+++ test.js
@@ -4,14 +4,14 @@ var isDark=true;
     if (isDark===true){
         document.getElementById("main").className="bg-gray-100 transition-all duration-1000";
         document.getElementById("button").className="rounded-xl m-10 hover:border-orange-400 hover:border-2 hover:bg-orange-300 cursor-pointer text-black p-2 bg-orange-600 transition-all duration-1000"
-        document.getElementsByTagName("h1")[0].innerText="Theme: Light"
+        document.getElementsByTagName("h1")[0].innerText="Light Theme"
         document.getElementsByTagName("h1")[0].className=" text-black transition ease-in-out duration-1000"
         isDark=false
     }
     else{
         document.getElementById("main").className="bg-gray-700 transition duration-1000";
-        document.getElementById("button").className="rounded-xl m-10 cursor-pointer hover:border-blue-400 hover:border-2 hover:bg-blue-300 bg-blue-600 p-2 text-white transition-all duration-1000"
-        document.getElementsByTagName("h1")[0].innerText="Theme: Dark"
+        document.getElementById("button").className="rounded-xl cursor-pointer hover:border-blue-400 hover:border-2 hover:bg-blue-300 bg-blue-600 p-2 text-white transition-all duration-1000"
+        document.getElementsByTagName("h1")[0].innerText="Dark Theme"
         document.getElementsByTagName("h1")[0].className=" text-white transition duration-1000"
         isDark=true
     }
