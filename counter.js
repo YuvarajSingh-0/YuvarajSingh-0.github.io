6bc98772ff28b67e38596e7d6f47bba85d2e23e9
yuvarajsingh-0 <yuvarajsingh170@gmail.com>
2022-01-07 01:33:30 +0530

    Added Various ways of counter.js and Some Color Changes

--- counter.js
+++ counter.js
@@ -1,19 +1,62 @@
-// var value=document.getElementById("display_num").innerHTML
 var count=0;
-// document.querySelector("#decrease").onclick=add(-1);
-// document.querySelector("#increase").addEventListener('click',add(1));
+
+//----------------------EventListener Usage especially in line 8,9-------------------
+
+// function setValue(a){
+//     count=count+a;
+//     console.log(count);
+//     document.getElementById("display_num").textContent=count
+// }
+// document.querySelector("#decrease").onclick=()=>{ setValue(-1) };
+// document.querySelector("#increase").addEventListener('click',()=>{ setValue(1) });
 // document.querySelector("#reset").addEventListener('click',function(){
 //     count=0
 //     console.log(count)
 //     document.getElementById("display_num").textContent=count
 // });
-function setValue(a) {
-    if (a===0) {
-        count=0
-    }
-    else{
-        count=count+(a)
-    }
-    console.log(count)
-    document.getElementById("display_num").textContent=count
-}
+
+//--------------------------------Using ForEach Loop-------------------------------
+
+document.querySelectorAll("button").forEach(function (button)  {
+    console.log(button)
+    button.addEventListener("click",function(e){
+        console.log(e);
+        const btn_id=e.currentTarget.getAttribute("id")
+        console.log(btn_id)
+        if(btn_id==="increase"){
+            count++;
+            console.log(count)
+        }
+        else if(btn_id==="decrease"){
+            count--;
+            console.log(count)
+        }
+        else{
+            count=0
+        }
+        if(count>0){
+            document.querySelector("#display_num").className="text-9xl font-bold text-green-500";
+        }
+        else if(count<0){
+            document.getElementById("display_num").className="text-9xl font-bold text-red-500";
+        }
+        else{
+            document.getElementById("display_num").className="text-9xl font-bold ";
+        }
+        document.getElementById("display_num").textContent=count
+    })
+});
+
+
+//------------------Inline Function Calling Below (must add onclick attribute to button tags)---------------
+
+// function setValue(a) {
+//     if (a===0) {
+//         count=0
+//     }
+//     else{
+//         count=count+(a)
+//     }
+//     console.log(count)
+//     document.getElementById("display_num").textContent=count
+// }
