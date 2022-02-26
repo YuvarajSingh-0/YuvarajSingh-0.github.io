cd5157b31dfa2a408d94b93a7eb11e3cd8daad33
yuvarajsingh-0 <yuvarajsingh170@gmail.com>
2022-02-27 01:12:08 +0530

    flex-wrap for mobille view and ignored nodemodules

--- clock.js
+++ clock.js
@@ -1,15 +1,17 @@
 var interval = setInterval(() => {
-    const d = new Date();
-    var minutes = d.getMinutes();
-    var hours = d.getHours() >= 12 ? d.getHours() - 12 : d.getHours();
-    var sec = d.getSeconds();
-    document.getElementById("time").innerText =
-      (hours < 10 ? "0" : "") +
-      hours +
-      " : " +
-      minutes +
-      " : " +
-      sec +
-      "  " +
-      (hours >= 12 ? "PM" : "AM");
-  }, 1000);  
+  const d = new Date();
+  var minutes = d.getMinutes();
+  var hours = d.getHours() >= 12 ? d.getHours() - 12 : d.getHours();
+  var sec = d.getSeconds();
+  document.getElementById("time").innerText =
+    (hours < 10 ? "0" : "") +
+    hours +
+    " : " +
+    (minutes < 10 ? "0" : "") +
+    minutes +
+    " : " +
+    (sec < 10 ? "0" : "") +
+    sec +
+    "  " +
+    (hours >= 12 ? "PM" : "AM");
+}, 1000);
